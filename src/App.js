13c119b37ca35f7fa61a072fd93cfe3020da534d
Kavyakrishnan <kavyakrishnan9821@gmail.com>
2022-05-25 18:12:52 +0530

    complete

--- src/App.js
+++ src/App.js
@@ -4,12 +4,18 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 import Signup from './Component/Signup';
 import Signin from './Component/Signin';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
    <div>
-     <Signup/>
-     <Signin/>
+     <BrowserRouter>
+     <Routes>
+       <Route path="/"exact element={<Signup/>}/>
+       <Route path="/search"exact element={<Signin/>}/>
+    
+
+       </Routes></BrowserRouter>
    </div>
   );
 }
